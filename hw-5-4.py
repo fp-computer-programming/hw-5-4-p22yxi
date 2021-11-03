cfa4cfa9396c5c32387828791d67a0d116f8e9cc
p22yxi <p22yxi@fairfieldprep.org>
2021-11-02 21:02:22 -0700

    hw5-4

---
+++ hw-5-4.py
@@ -0,0 +1,11 @@
+# Author Yongdong Xi
+
+# Question 1
+fruit = "Python"
+charactor = fruit[2]
+print(charactor)
+
+# Question 2
+string1 = "Strings are simply a sequence of characters"
+charactor = string1[31]
+print(charactor)
