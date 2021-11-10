d05fc1f785af841ec356c0cc249b806158500268
p22yxi <p22yxi@fairfieldprep.org>
2021-11-10 03:46:20 -0800

    hw5-4 finial

--- hw-5-4.py
+++ hw-5-4.py
@@ -7,5 +7,31 @@ print(charactor)
 
 # Question 2
 string1 = "Strings are simply a sequence of characters"
-charactor = string1[31]
+charactor = string1[30]
 print(charactor)
+
+# Question 3
+sentence = "excellent"
+print(len(sentence))
+
+# Question 4
+print("apple" in "pineapple") 
+
+# Question 5
+print("Peach" > "pear") 
+
+# Question 6
+sentence1 = input("Give me a sentence")
+print(100 * sentence1)
+
+# Question 7
+print('p' not in "pineapple")
+
+# Question 8
+print('54321')
+
+# Question 9
+print('\"Livin\' On a Prayer\" is my friend\'s favorite song.')
+
+# Question 10
+print(len("my lover\'s eyes never be the sun"))
